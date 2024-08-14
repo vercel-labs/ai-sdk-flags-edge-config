06f6daf79e8cd6f7b8e489132f793297aee8672e
Nico Albanese <nico.albanese@vercel.com>
2024-08-14 10:15:43 +0200

    update flags implementation for vc toolbar

--- flags.ts
+++ flags.ts
@@ -1,10 +1,15 @@
 import { get } from "@vercel/edge-config";
 import { unstable_flag as flag } from "@vercel/flags/next";
 
-export const modelId = flag({
+export const modelId = flag<string>({
   key: "model_id",
+  options: [
+    { label: "gpt-4o", value: "openai:gpt-4o" },
+    { label: "llama 3.1 8b", value: "groq:llama-3.1-8b-instant" },
+  ],
   async decide() {
-    const value = await get(this.key);
+    const value = (await get(this.key)) as string;
     return value ?? "openai:gpt-4o";
   },
 });
+
