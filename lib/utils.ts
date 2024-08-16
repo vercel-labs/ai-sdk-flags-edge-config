a763fd50546c29df64f173c4c789884a50dc6539
Nico Albanese <nico.albanese@vercel.com>
2024-08-16 18:21:44 +0200

    update lib logic

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export const disableApp = process.env.VERCEL_URL === "dynamic-model-ai-sdk.vercel.app" ?? false;
+// disable production demo app by default
+export const disableApp = process.env.NEXT_PUBLIC_DISABLE_APP
+  ? Boolean(process.env.NEXT_PUBLIC_DISABLE_APP)
+  : false;
