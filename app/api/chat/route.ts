ef20eced4041b95b79d4db525dd26a2512a7b472
Nico Albanese <nico.albanese@vercel.com>
2024-12-20 19:03:22 +0100

    chore: update to version 4

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -8,10 +8,10 @@ export async function POST(req: Request) {
   const { messages } = await req.json();
   const model = await modelId() as string;
 
-  const result = await streamText({
+  const result = streamText({
     model: registry.languageModel(model),
     messages: convertToCoreMessages(messages),
   });
 
-  return result.toAIStreamResponse();
+  return result.toDataStreamResponse();
 }
